c957b21aacec981455d4beb7c7f6afc9c0be07db
Nick Wehner <nicholas.wehner@gmail.com>
2022-09-06 07:56:05 -0700

    Save WIP

--- download_engrxiv.sh
+++
@@ -1,7 +0,0 @@
-csvtool col 1 engrxiv-papers.csv > junk.csv
-csvtool drop 1 junk.csv > guids.csv
-while IFS=, read GUID
-do
-    wget https://osf.io/$GUID/download -O $GUID.pdf
-done < guids.csv
-rm junk.csv
